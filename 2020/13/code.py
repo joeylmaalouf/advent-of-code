4c79c2dc691742413b984d53d7a92747e36b1709
joeylmaalouf <joeylmaalouf@gmail.com>
2020-12-22 14:56:54 -0500

    day 13 part 2

--- 2020/13/code.py
+++ 2020/13/code.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+from fractions import gcd
 from math import ceil
 
 v2 = False # change to True if doing the second version of the puzzle
@@ -10,24 +11,34 @@ earliest, busids = filehandle.read().strip().split('\n')
 earliest = int(earliest)
 filehandle.close()
 
+def lcm(a, b):
+	return abs(a * b) // gcd(a, b)
+
 if v2:
 	busids = busids.split(',')
 	filtered = []
-	for index, busid in zip(range(0, len(busids)), busids):
+	for offset, busid in zip(range(0, len(busids)), busids):
 		if busid != 'x':
-			filtered.append((index, int(busid)))
+			filtered.append((offset, int(busid)))
 	busids = filtered
 	increment = busids[0][1]
 	timestamp = 0
+	furthest = 0
 	while True:
 		timestamp += increment
+		progress = 0
 		valid = True
-		for index, busid in busids:
-			if (timestamp + index) % busid != 0:
+		for offset, busid in busids:
+			if (timestamp + offset) % busid != 0:
 				valid = False
 				break
+			progress += 1
 		if valid:
 			break
+		if progress > furthest:
+			furthest = progress
+			if progress >= 2:
+				increment = reduce(lcm, [busid for offset, busid in busids[:progress]], 1)
 	print timestamp
 else:
 	busids = [int(busid) for busid in busids.split(',') if busid != 'x']
